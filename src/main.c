d2ea9879a7592bc789feb1812c1ecc5c80586d84
Olof Johnsson <olofjohnsson@EC365-681822.ecdomain.net>
2025-10-28 07:19:56 +0100

    Adding entrance gate application

--- src/main.c
+++ src/main.c
@@ -1,6 +1,18 @@
 #include <zephyr/kernel.h>
+#include <zephyr/gpio.h>
+
+#define NODE_MP3_SW DT_NODELABEL(sw_mp3)
+static const struct gpio_dt_spec mp3 = GPIO_DT_SPEC_GET(NODE_MP3_SW, gpios);
+
+int init_pins(void)
+{
+  int ret = gpio_pin_configure_dt(&mp3, GPIO_OUTPUT_ACTIVE);
+}
 
 int main(void)
 {
-        return 0;
+  if (!gpio_is_ready_dt(&mp3)) {
+    return;
+  }
+  return 0;
 }
